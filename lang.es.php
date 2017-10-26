0d46bee079287c61de80c957d44d9bc0b132fd50
langerk <k.langerak@hva.nl>
2017-10-26 13:05:38 +0200

    Talen toevoegen

---
+++ lang.es.php
@@ -0,0 +1,32 @@
+<?php
+//Spaanse vertaling
+$language["SITE_TITLE"] = "Dreambox";
+$language["HOLIDAY_PHOTO_VIEWER"] = "Visor de fotos de vacaciones";
+$language["HOME"] = "Home;";
+$language["ABOUT"] = "About";
+$language["CONTACT"] = "Contacto";
+$language["WELCOME"] = "Bienvenido al visor de fotos de vacaciones";
+$language["ENTER_CODE"] = "ENTRA TU CÓDIGO";
+$language["SUBMIT_CODE"] = "INGRESE EL CÓDIGO";
+$language["MAP_PHOTO_BOOTH"] = "Mapa de las fotomatoneras";
+$language["COPYRIGHT"] = "Copyright © Corendon | Creado por Resort5 2017";
+$language["SLIDE1"] = "Primera Dia";
+$language["SLIDE1_CAPTION"] = "Recordatorios para nunca olvidar";
+$language["SLIDE2"] = "Segunda Dia";
+$language["SLIDE2_CAPTION"] = "De nuevo, eso siempre queda contigo";
+$language["SLIDE3"] = "Tercera Dia";
+$language["SLIDE3_CAPTION"] = "Corendon y las vacaciones están inextricablemente vinculadas"
+$language["ABOUT_TITLE"] = "OSobre el sitio web";
+$language["ABOUT_TEXT"] = "En el sitio web, puede ver las fotos que ha realizado en la sesión de fotos. Fotos guardadas por hasta un año. También tiene la opción de editar sus fotos agregando filtros, por ejemplo. Puede ver las fotos de 2 maneras: mediante un código QR y mediante un código personal.";
+$language["CONTACT_TITLE"] = "Contacto";
+$language["CONTACT_PHONE"] = "Número de teléfono:";
+$language["CONTACT_EMAIL"] = "Email";
+$language["CONTACT_ADDRESS"] = "Dirección:";
+$language["CONTACT_FORM_TITLE"] = "Formulario de contacto";
+$language["CONTACT_FORM_NAME"] = "Nombre";
+$language["CONTACT_FORM_EMAIL"] = "Email";
+$language["CONTACT_FORM_PHONE"] = "Número de teléfono";
+$language["CONTACT_FORM_MESSAGE"] = "Mensaje";
+$language["ERR_NO_CODE"] = "Sin código ingresado";
+
+?>
---
+++ lang.nl.php
@@ -0,0 +1,32 @@
+<?php
+//Nederlandse vertaling
+$language["SITE_TITLE"] = "Dreambox";
+$language["HOLIDAY_PHOTO_VIEWER"] = "Vakantie Foto Viewer";
+$language["HOME"] = "Home;";
+$language["ABOUT"] = "About";
+$language["CONTACT"] = "Contact";
+$language["WELCOME"] = "Welkom bij de Vakantie Foto Viewer";
+$language["ENTER_CODE"] = "VOER JE CODE IN";
+$language["SUBMIT_CODE"] = "SUBMIT CODE";
+$language["MAP_PHOTO_BOOTH"] = "Kaart van foto booths";
+$language["COPYRIGHT"] = "Copyright © Corendon | Gemaakt door Resort5 2017";
+$language["SLIDE1"] = "Eerste Slide";
+$language["SLIDE1_CAPTION"] = "Herinneringen om nooit meer te vergeten";
+$language["SLIDE2"] = "Tweede Slide";
+$language["SLIDE2_CAPTION"] = "Weer dat altijd bij je blijft";
+$language["SLIDE3"] = "Derde Slide";
+$language["SLIDE3_CAPTION"] = "Corendon en vakantie zijn onlosmakelijk verbonden"
+$language["ABOUT_TITLE"] = "Over de website";
+$language["ABOUT_TEXT"] = "Op deze website kan je de foto's inzien die u op de fotobooth gemaakt heeft. Foto's maximaal een jaar bewaard. U heeft ook de mogelijkheid om uw foto's te bewerken door bijvoorbeeld filters toe te voegen. U kan de foto's op 2 manieren inzien: doormiddel van een QR-code en doormiddel van een persoonlijke code.";
+$language["CONTACT_TITLE"] = "Contact";
+$language["CONTACT_PHONE"] = "Telefoon:";
+$language["CONTACT_EMAIL"] = "Email";
+$language["CONTACT_ADDRESS"] = "Address:";
+$language["CONTACT_FORM_TITLE"] = "CONTACT FORMULIER";
+$language["CONTACT_FORM_NAME"] = "Naam";
+$language["CONTACT_FORM_EMAIL"] = "Email Address";
+$language["CONTACT_FORM_PHONE"] = "Telefoon Nummer";
+$language["CONTACT_FORM_MESSAGE"] = "Bericht";
+$language["ERR_NO_CODE"] = "Geen code ingevoerd";
+
+?>
