aec50b561de9d3ad6f1881b7b25796d7430b06a1
A.B. Espinosa Encarnación <alfred.espinosa.encarnacion@hva.nl>
2018-01-17 21:10:28 +0100

    Verbeterde mail systeem

---
+++ Interface/NewPhotoboothUI/src/database_Mail/EmailSend.java
@@ -0,0 +1,58 @@
+package database_Mail;
+
+/**
+ *
+ * @author Alfred Espinosa
+ */
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.internet.MimeMessage;
+
+public class EmailSend {
+
+    private static final String HOST = "smtp.gmail.com";
+    private static final String USER = "alfredez008@gmail.com";
+    private static final String PASS = "Omegazer0";
+    private static final String FROM = "john@doe.com";
+    private static final String SUBJECT = "Corendon Resort - Dreambox";
+    private static final boolean SESSION_DEBUG = false;
+    
+    public static void Mail(String email, String code) {
+        try {
+            String to = email;
+            String messageText = "Thank you for using the Dreambox!\n Visit our website to "
+                    + "see the pictures at www.mustafayucesan.nl?code="+code + "\n\n With"
+                    + "Kind of Regards,\n Corendon";
+            
+
+            Properties props = System.getProperties();
+
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", HOST);
+            props.put("mail.smtp.port", "587");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.required", "true");
+
+            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(SESSION_DEBUG);
+            Message msg = new MimeMessage(mailSession);
+            msg.setFrom(new InternetAddress(FROM));
+            InternetAddress[] address = {new InternetAddress(to)};
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject(SUBJECT);
+            msg.setSentDate(new Date());
+            msg.setText(messageText);
+
+            Transport transport = mailSession.getTransport("smtp");
+            transport.connect(HOST, USER, PASS);
+            transport.sendMessage(msg, msg.getAllRecipients());
+            transport.close();
+            System.out.println("message send successfully");
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+    }
+}
