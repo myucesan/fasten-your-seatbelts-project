7074a048b29e67607e165c52b93439266f59af5c
A.B. Espinosa Encarnación <alfred.espinosa.encarnacion@hva.nl>
2018-01-17 10:45:30 +0100

    Nieuwe versie - Final toegevoegd

--- Interface/NewPhotoboothUI/src/newphotoboothui/FTPUploader.java
+++ Interface/NewPhotoboothUI/src/newphotoboothui/FTPUploader.java
@@ -11,13 +11,20 @@ import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
+/**
+ *
+ * @author Alfred Espinosa
+ */
 public class FTPUploader {
 
     FTPClient ftp = null;
-    static boolean delete = false;
-    static String Path = "C:\\Users\\Alfred Espinosa\\Desktop\\ftytest.txt";
+    private static final String PATH = "F:\\HvA\\Homework\\Programming\\NewPhotoboothUI\\";
+    private static final String IMAGE_DIR = "/var/www/html/images/";
+    private static final String HOST = "185.177.59.153";
+    private static final String USER = "fys";
+    private static final String PASSWORD = "fys-resort5";
 
-    public FTPUploader(String host, String user, String pwd) throws Exception {
+    private FTPUploader(String host, String user, String pwd) throws Exception {
         ftp = new FTPClient();
         ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
         int reply;
@@ -32,15 +39,18 @@ public class FTPUploader {
         ftp.enterLocalPassiveMode();
     }
 
-    public void uploadFile(String localFileFullName, String fileName, String hostDir)
+    private void uploadFile(String localFileFullName, String fileName, String hostDir)
             throws Exception {
         try (InputStream input = new FileInputStream(new File(localFileFullName))) {
             this.ftp.storeFile(hostDir + fileName, input);
+
+            this.ftp.sendCommand("SITE chmod 755 " + IMAGE_DIR + fileName + "");
+
             //delete = true;
         }
     }
 
-    public void disconnect() {
+    private void disconnect() {
         if (this.ftp.isConnected()) {
             try {
                 this.ftp.logout();
@@ -51,35 +61,38 @@ public class FTPUploader {
         }
     }
 
-    public static void Upload(String Path2) throws Exception {
+    public static void Upload(String fileName) throws Exception {
         System.out.println("Start");
-        FTPUploader ftpUploader = new FTPUploader("185.177.59.153", "fys", "fys-resort5");
+
+        FTPUploader ftpUploader = new FTPUploader(HOST, USER, PASSWORD);
         //FTP server path is relative. So if FTP account HOME directory is "/home/pankaj/public_html/" and you need to upload 
         // files to "/home/pankaj/public_html/wp-content/uploads/image2/", you should pass directory parameter as "/wp-content/uploads/image2/"
-        ftpUploader.uploadFile(Path2, "Thomas.jpg", "/var/www/");
+        System.out.println(fileName);
+        ftpUploader.uploadFile(PATH + fileName, fileName, IMAGE_DIR);
         //deleteFile();
         ftpUploader.disconnect();
+
         System.out.println("Done");
     }
 
-    public static void deleteFile() {
-        if (delete) {
-            try {
+    public static void deleteFile(String fileName) {
 
-                File file = new File(Path);
+        try {
 
-                if (file.delete()) {
-                    System.out.println(file.getName() + " is deleted!");
-                } else {
-                    System.out.println("Delete operation is failed.");
-                }
+            File file = new File(PATH + fileName);
 
-            } catch (Exception e) {
-
-                e.printStackTrace();
-
+            if (file.delete()) {
+                System.out.println(file.getName() + " is deleted from the pi dir!");
+            } else {
+                System.out.println("Delete operation is failed.");
             }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
         }
+
     }
 
 }
